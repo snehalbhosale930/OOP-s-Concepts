80b03edc45ff5adbd73e18bda586bd72f3e53074
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-03-25 11:27:51 +0530

    Add files via upload
    In this Notification File there is code for send notification.

---
+++ Notification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPs__Concepts
+{
+    public class Notification
+    {
+        public virtual void SendNotification()
+        {
+            Console.WriteLine(" Test notification");
+            Console.WriteLine("this is base class method and does not send any notification");
+        }
+    }
+
+    public class SMS : Notification
+    {
+        public override void SendNotification()
+        {
+            Console.WriteLine("SMS is Sending");
+            Console.WriteLine("SMS Sent");
+        }
+    }
+
+    public class Email : Notification
+    {
+        public override void SendNotification()
+        {
+            Console.WriteLine("Email is Sending");
+            Console.WriteLine("Email Sent");
+        }
+    }
+}
