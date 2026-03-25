43c5d1b3c2c43b68523f884127320ab2353a54dc
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-03-25 11:30:03 +0530

    Add files via upload
    In this students file there is code for showing student id, name, email, etc.

---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOPs__Concepts
+{
+    public class Student
+    {
+        public string istrName;
+        public int iintId;
+        public string istrEmail;
+
+        
+        public void getvalue(int aintId, string astrName, string astrEmail=null)
+        {
+            istrName = astrName;
+            iintId = aintId;
+            istrEmail = astrEmail;
+        }
+        public void printvalues()
+        {
+            Console.WriteLine(iintId + " " + istrName + " " + istrEmail);
+        }
+         
+    }  
+
+}
+
