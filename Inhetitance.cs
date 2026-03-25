fdd1aba88209b56b0e297780b84b847d46fe6d6f
snehalbhosale930 <snehalbhosale930@gmail.com>
2026-03-25 11:26:45 +0530

    Add files via upload
    In this Inheritance File there is code that shows how is the inheritance flow.

---
+++ Inhetitance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPs__Concepts
+{
+    public class Person
+    {
+        private DateTime idtDOB;
+
+        protected string istrGender;
+
+        internal string istrMobile;
+
+        public string istrFirstName;
+        public string istrLastName;
+        public string istrEmail;
+
+
+        virtual public void show()
+        {
+            Console.WriteLine("i am inside of show method of class Person");
+        }
+    }
+    public class student : Person 
+    {
+        public override void show()
+        {
+            Console.WriteLine("i am inside of show method of class Student");
+        }
+    }
+    public class OutSideStudent : Person
+    {
+        
+        public  override void show()
+        {
+            Console.WriteLine("i am inside of show method of class OutSideStudent");
+        }
+    }
+    public class Staff : Person
+    {
+        public override void show()
+        {
+            Console.WriteLine("i am inside the show method of class Staff");
+        }
+    }
+}
